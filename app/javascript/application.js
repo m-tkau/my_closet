717da039190985377107b44e13faaf79031f6b8c
takumi <takumi@takumi.local>
2025-04-29 10:50:51 +0900

    ログイン、ログアウト

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails";  // Turbo Railsのインポート
-import * as bootstrap from "bootstrap";  // Bootstrapのインポート
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import Rails from "@rails/ujs";
+Rails.start();
