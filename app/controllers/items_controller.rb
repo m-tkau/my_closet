9febf70b33b5acc25602ad178d6d953f8a58e4b5
takumi <takumi@takumi.local>
2025-05-01 22:25:44 +0900

    アイテム一覧、編集、削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :set_item, only: %i[show edit update destroy]
+
   def index
-    @items = Item.all.order(created_at: :desc)  # 新しい順に表示
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def show
+    # 詳細ページ用（@itemはbefore_actionで取得済み）
   end
 
   def new
@@ -16,10 +23,30 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    # 編集ページ用
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: 'アイテムを更新しました！'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: 'アイテムを削除しました。'
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :category, :tag, :price, :purchased_at, :shop, :photo)
   end
 end
-  
